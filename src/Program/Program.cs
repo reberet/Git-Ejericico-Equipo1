cf036b7a68cf2419870a4747d008b6cbfeee891b
Renzo <renzoberetta27@gmail.com>
2026-03-24 21:54:47 -0300

    add

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,7 +4,7 @@
     {
         Console.WriteLine(Suma.Sumar(1, 2));
         Console.WriteLine(Resta.Restar(3, 4));
-        Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
-        Console.WriteLine(Division.Dividir(7, 8));
+        Console.WriteLine(Multiplication.Multiply(5, 6));
+        Console.WriteLine(Division.Divide(7, 8));
     }
 }
