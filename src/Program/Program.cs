3b197214346089fb5537f90a58d29205839e5a03
Renzo <renzoberetta27@gmail.com>
2026-03-27 19:02:48 -0300

    cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,8 +3,12 @@
     public static void Main()
     {
         Console.WriteLine(Suma.Sumar(1, 2));
-        Console.WriteLine(Resta.Restar(3, 4));
+        Console.WriteLine(Substraction.Substract(3, 4));
         Console.WriteLine(Multiplication.Multiply(5, 6));
-        Console.WriteLine(Division.Divide(7, 8));
+        Console.WriteLine(Division.Divide(7, 8)); cambio incorrecto 
     }
+
+
 }
+
+ 
